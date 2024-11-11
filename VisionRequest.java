e68332d35ca179a8e64a2f8f70ff117d5e98bb28
Senthil velan <121471306+senthilpuli@users.noreply.github.com>
2024-11-11 19:27:20 +0530

    Create VisionRequest.java

---
+++ VisionRequest.java
@@ -0,0 +1,9 @@
+public class VisionRequest {
+    private String prompt;
+    private String image;
+
+    public VisionRequest(String prompt, String image) {
+        this.prompt = prompt;
+        this.image = image;
+    }
+}
